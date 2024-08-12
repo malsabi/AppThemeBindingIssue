66786ed6a59f18544f7972509d9d2b757f1975fd
malsa <malsa@MALSABI>
2024-08-12 07:47:28 +0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppThemeBindingIssue
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
